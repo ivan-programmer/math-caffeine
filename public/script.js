e45d3dbb569cd73a86dc6fa37b111d931b34f3c5
Ivan Vinogradov <ivan.web-developer@ya.ru>
2018-01-07 01:29:20 +0300

    added greeting

--- public/script.js
+++ public/script.js
@@ -1,4 +1,31 @@
 (function() {
-  alert('easy');
+  var App, doc, win;
+
+  doc = document;
+
+  win = window;
+
+  App = (function() {
+    function App() {
+      this.onStart();
+    }
+
+    App.prototype.onStart = function() {
+      setTimeout(function() {
+        return $('#greeting').fadeOut('slow');
+      }, 2500);
+      setTimeout(function() {
+        return $('#greeting-wrapper').fadeOut('slow');
+      }, 3000);
+      return setTimeout(function() {
+        return $('#greeting-wrapper').remove();
+      }, 3500);
+    };
+
+    return App;
+
+  })();
+
+  new App;
 
 }).call(this);
--- source/script.coffee
+++ source/script.coffee
@@ -1,1 +1,18 @@
-alert 'easy'
+doc = document
+win = window
+
+class App
+  constructor: ->
+    do @onStart
+  onStart: ->
+    setTimeout ->
+      $('#greeting').fadeOut 'slow'
+    , 2500
+    setTimeout ->
+      $('#greeting-wrapper').fadeOut 'slow'
+    , 3000
+    setTimeout ->
+      do $('#greeting-wrapper').remove
+    , 3500
+
+new App
